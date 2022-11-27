452c5bfda31ce54f52b7bf761bac6b718e3770bc
AppiahEnoch <prignutt@gmail.com>
2022-11-27 02:51:06 -1200

    all set for railway

--- config.php
+++ config.php
@@ -6,28 +6,23 @@ $password = "";
 $database = "gnatoc";
 
 /*
+// heroku
 $hostname = "us-cdbr-east-06.cleardb.net";
 $username = "b8052e1675b349";
 $password = "226c3dab";
 $database = "heroku_d0301c5e9ed1a1c";
 //*/
 //*
+
+// railway
+//*
 $hostname = "containers-us-west-98.railway.app";
 $username = "root";
 $password = "ismm28lwOe2ySW0pZhpq";
 $database = "railway";
-
-
-$hostname = $_ENV["HOST"];
-$username =$_ENV["USER"];
-$password =$_ENV["PASSWORD"];
-$database =$_ENV["DB"];
-$port =$_ENV["PORT"];
-
-//
-
-
+$port = "6385";
 //*/
+
 $conn = mysqli_connect($hostname, $username, $password, $database,$port) or die("Database connection failed");
 
 ?>
