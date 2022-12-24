50163bd232fa3864e36f050147e48fe252522d06
AppiahEnoch <prignutt@gmail.com>
2022-12-23 14:27:16 -1200

    3

--- upF1.php
+++ upF1.php
@@ -1,34 +1,42 @@
 <?php
 
-    $valid_extensions = array('jpeg', 'jpg', 'png', 'gif', 'bmp' , 'pdf' , 'doc' ); // valid extensions
- 
-    $valid_extensions2 = array('pdf' , 'doc' ); // valid extensions PDF ONLY
+
+$ext_doc = array('pdf' , 'doc' ); // valid extensions PDF ONLY
  
     include_once 'config.php';
     session_start();
-    //$_SESSION["staffID"]=1220016;
+   // $_SESSION["staffID"]=1220016;
     $fileD=$_SESSION["staffID"];
     $staffID=$_SESSION["staffID"];
+
+    try {
+        $id=  trim($staffID);
+
+        if(strlen($id)<1){
+            exit();
+        }
+    } catch (\Throwable $th) {
+        //throw $th;
+    }
+
+  
     
     $v1="file";
-   
-    $col=$_POST["text"];
+    $v2="text";
 
-    $newPath="";
+    $col=$_POST[$v2];
 
-   // echo 1;
-    //exit();
    
 
 
+    $_path="file";
 
 //  copy paste session
-    $path = 'upload/studyLeave/';
-    $fileName = $_FILES[$v1]['name'];
-    $tmp = $_FILES[$v1]['tmp_name'];
-    $err = $_FILES[$v1]['error'];
-   
-    $newPath=getFilepath();
+$fileName = $_FILES[$v1]['name'];
+$tmp = $_FILES[$v1]['tmp_name'];
+$newPath=getFilepath_doc();
+
+
     
 
     
@@ -43,12 +51,12 @@
       
 // prepare and bind
 try{
-    $sql = "UPDATE file SET $col=? WHERE staffID=?";
+    $sql = "UPDATE `file` SET $col=? WHERE staffID=?";
     $stmt = $conn->prepare($sql); 
     $stmt->bind_param("ss", $newPath,$staffID);
     $stmt->execute();
 
-  echo $newPath;
+     echo $newPath;
  
   
   $stmt->close();
@@ -71,56 +79,30 @@ try{
 
 
 
-function getFilepath(){
-  global $fileName,$tmp,$path,$valid_extensions,$fileD;
 
-  try {
-       // get uploaded file's extension
-       $ext = strtolower(pathinfo($fileName, PATHINFO_EXTENSION));
-       // can upload same image using rand function
-       $final_image = rand(1000,1000000).$fileD.$fileName;
-      // $final_image =$fileD.$fileName;
-       // check's valid format
-
-       if(!(in_array($ext, $valid_extensions))){
-        exit();
-       }
-       
-       $path = $path.strtolower($final_image); 
-       move_uploaded_file($tmp,$path);
-       
-       return $path;
-  } catch (\Throwable $th) {
-    //throw $th;
-  }
-   
+  function getFilepath_doc(){
+    global $fileName,$tmp,$ext_doc,$fileD,$_path;
+  
+    try {
+         // get uploaded file's extension
+         $ext = strtolower(pathinfo($fileName, PATHINFO_EXTENSION));
+         // can upload same image using rand function
+         $final_image = rand(1000,1000000).$fileD.$fileName;
+        // $final_image =$fileD.$fileName;
+         // check's valid format
+  
+         if(!(in_array($ext, $ext_doc))){
+          exit;
+         }
+         $final_path = $_path. '/'.strtolower($final_image); 
+         move_uploaded_file($tmp, $final_path);
+         
+         return  $final_path;
+    } catch (\Throwable $th) {
+      throw $th;
+    }
      
-}
-    
-
-function getFilepath2(){
-  global $fileName,$tmp,$path,$valid_extensions2,$fileD;
-
-  try {
-       // get uploaded file's extension
-       $ext = strtolower(pathinfo($fileName, PATHINFO_EXTENSION));
-       // can upload same image using rand function
-       $final_image = rand(1000,1000000).$fileD.$fileName;
-      // $final_image =$fileD.$fileName;
-       // check's valid format
-
-       if(!(in_array($ext, $valid_extensions2))){
-        exit();
-       }
        
-       $path = $path.strtolower($final_image); 
-       move_uploaded_file($tmp,$path);
-       
-       return $path;
-  } catch (\Throwable $th) {
-    //throw $th;
   }
-   
-     
-}
+
     
