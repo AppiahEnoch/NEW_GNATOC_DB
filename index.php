0616285da76b68a3808cda9f896baf9306b003cf
AppiahEnoch <prignutt@gmail.com>
2023-01-09 02:00:22 -1200

    reloaded

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
+
+<?php
+  session_start();
+  session_destroy();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -77,10 +83,7 @@
     });
     </script>
 
-<?php
-  session_start();
-  session_destroy();
-?>
+
 
 
 </head>
