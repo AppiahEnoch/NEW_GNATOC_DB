39b5d23cae08e57ba1a60610802f141d2d98b3f0
AppiahEnoch <prignutt@gmail.com>
2023-01-22 16:14:11 -1200

    88888

--- getData.php
+++ getData.php
@@ -116,18 +116,19 @@ if ($result->num_rows > 0) {
 
 
 
-
+    $return_arr[]= array(
+      "staffID" => $staffID,
+      "fullName" => $fullName,
+      "mobile" => $mobile
+  );
+  
   }
 }
 
 
 
 
-  $return_arr[]= array(
-    "staffID" => $staffID,
-    "fullName" => $fullName,
-    "mobile" => $mobile
-);
+
 
   $json = json_encode($return_arr);
   if(json_last_error() === JSON_ERROR_NONE){
