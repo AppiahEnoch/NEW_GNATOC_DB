0cfc72bfb9e0eba2453b58f83d12a2b8892564fd
AppiahEnoch <prignutt@gmail.com>
2023-01-26 02:28:53 -1200

    keep5

--- record.php
+++ record.php
@@ -108,7 +108,7 @@
           success: function (response) {
             if (aeEmpty(response)) {
               hideSpin();
-              alert("1__" + response);
+             // alert("1__" + response);
               return;
             }
 
