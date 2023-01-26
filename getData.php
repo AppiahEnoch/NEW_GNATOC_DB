459cb790a67da9d2653952f64edfce718f787b8b
AppiahEnoch <prignutt@gmail.com>
2023-01-25 22:55:37 -1200

    ttttt

--- getData.php
+++ getData.php
@@ -11,26 +11,26 @@ error_reporting(E_ALL);
 $spreadsheet = new PhpOffice\PhpSpreadsheet\Spreadsheet();
 $spreadsheet->setActiveSheetIndex(0);
 $activeSheet = $spreadsheet->getActiveSheet();
-$activeSheet ->getColumnDimension('A')->setAutoSize(true);
-$activeSheet ->getColumnDimension('B')->setAutoSize(true);
-$activeSheet ->getColumnDimension('C')->setAutoSize(true);
-$activeSheet ->getColumnDimension('D')->setAutoSize(true);
-$activeSheet ->getColumnDimension('E')->setAutoSize(true);
-$activeSheet ->getColumnDimension('F')->setAutoSize(true);
-$activeSheet ->getColumnDimension('G')->setAutoSize(true);
-$activeSheet ->getColumnDimension('H')->setAutoSize(true);
-$activeSheet ->getColumnDimension('I')->setAutoSize(true);
-$activeSheet ->getColumnDimension('J')->setAutoSize(true);
-$activeSheet ->getColumnDimension('K')->setAutoSize(true);
-$activeSheet ->getColumnDimension('L')->setAutoSize(true);
-$activeSheet ->getColumnDimension('M')->setAutoSize(true);
-$activeSheet ->getColumnDimension('N')->setAutoSize(true);
-$activeSheet ->getColumnDimension('O')->setAutoSize(true);
-$activeSheet ->getColumnDimension('P')->setAutoSize(true);
-$activeSheet ->getColumnDimension('Q')->setAutoSize(true);
-$activeSheet ->getColumnDimension('R')->setAutoSize(true);
-$activeSheet ->getColumnDimension('S')->setAutoSize(true);
-$activeSheet ->getColumnDimension('T')->setAutoSize(true);
+$activeSheet->getColumnDimension('A')->setAutoSize(true);
+$activeSheet->getColumnDimension('B')->setAutoSize(true);
+$activeSheet->getColumnDimension('C')->setAutoSize(true);
+$activeSheet->getColumnDimension('D')->setAutoSize(true);
+$activeSheet->getColumnDimension('E')->setAutoSize(true);
+$activeSheet->getColumnDimension('F')->setAutoSize(true);
+$activeSheet->getColumnDimension('G')->setAutoSize(true);
+$activeSheet->getColumnDimension('H')->setAutoSize(true);
+$activeSheet->getColumnDimension('I')->setAutoSize(true);
+$activeSheet->getColumnDimension('J')->setAutoSize(true);
+$activeSheet->getColumnDimension('K')->setAutoSize(true);
+$activeSheet->getColumnDimension('L')->setAutoSize(true);
+$activeSheet->getColumnDimension('M')->setAutoSize(true);
+$activeSheet->getColumnDimension('N')->setAutoSize(true);
+$activeSheet->getColumnDimension('O')->setAutoSize(true);
+$activeSheet->getColumnDimension('P')->setAutoSize(true);
+$activeSheet->getColumnDimension('Q')->setAutoSize(true);
+$activeSheet->getColumnDimension('R')->setAutoSize(true);
+$activeSheet->getColumnDimension('S')->setAutoSize(true);
+$activeSheet->getColumnDimension('T')->setAutoSize(true);
 
 
 $activeSheet->setTitle('records');
@@ -68,12 +68,12 @@ $activeSheet->setCellValue('T1', 'RANK');
 
 
 
-$sex=$_POST["sex"];
-$from=$_POST["from"];
-$to=$_POST["to"];
-$level=$_POST["level"];
-$rank1=$_POST["rank1"];
-$program=$_POST["program"];
+$sex = $_POST["sex"];
+$from = $_POST["from"];
+$to = $_POST["to"];
+$level = $_POST["level"];
+$rank1 = $_POST["rank1"];
+$program = $_POST["program"];
 
 $sex = mysqli_real_escape_string($conn, $sex);
 $from = mysqli_real_escape_string($conn, $from);
@@ -82,40 +82,40 @@ $level = mysqli_real_escape_string($conn, $level);
 $rank1 = mysqli_real_escape_string($conn, $rank1);
 $program = mysqli_real_escape_string($conn, $program);
 
-if($rank1=="none"){
+if ($rank1 == "none") {
   $rank1 = "";
 }
 
-if($program=="none"){
-  $program ="";
+if ($program == "none") {
+  $program = "";
 }
 
 
 $filters = array();
 if (!empty($sex)) {
-    $filters[] = "gender = '$sex'";
+  $filters[] = "gender = '$sex'";
 }
 if (!empty($from)) {
-    $filters[] = "yearOfAdmission = '$from'";
+  $filters[] = "yearOfAdmission = '$from'";
 }
 if (!empty($to)) {
-    $filters[] = "yearOfCompletion = '$to'";
+  $filters[] = "yearOfCompletion = '$to'";
 }
 if (!empty($level)) {
-    $filters[] = " `level` = '$level'";
+  $filters[] = " `level` = '$level'";
 }
 if (!empty($rank1)) {
-    $filters[] = " `rank` =   '$rank1'";
+  $filters[] = " `rank` =   '$rank1'";
 }
 
 if (!empty($program)) {
-    $filters[] = "`course` = '$program'";
+  $filters[] = "`course` = '$program'";
 }
 
 $query = "SELECT * FROM memberbio";
 
 if (!empty($filters)) {
-    $query .= " WHERE " . implode(" AND ", $filters);
+  $query .= " WHERE " . implode(" AND ", $filters);
 }
 
 
@@ -123,86 +123,86 @@ if (!empty($filters)) {
 
 //$query = "SELECT * FROM memberbio";
 // Execute the query
-$result = mysqli_query($conn,$query);
+$result = mysqli_query($conn, $query);
 
 if (!$result) {
- // die("Error: " . mysqli_error($conn));
+  // die("Error: " . mysqli_error($conn));
 }
 
 
-$row2=2;
+$row2 = 2;
 
 if ($result->num_rows > 0) {
   // output data of each row
   while ($row = $result->fetch_assoc()) {
 
 
-    $staffID=$row["staffID"];
-    $fName=$row["fName"];
-    $mName=$row["mName"];
-    $lName=$row["lName"];
-    $mobile=$row["mobile"];
-    $fullName=$fName." ".$mName." ".$lName;
-
-
-    
-    $gender=$row["gender"];
-    $ghanaCard=$row["ghanaCard"];
-    $email=$row["email"];
-    $ssnit=$row["ssnitNumber"];
-    $studyLeaveStatus=$row["studyLeaveStatus"];
-    $admissionNumber=$row["admissionNumber"];
-    $regNumber=$row["regNumber"];
-    $level=$row["level"];
-    $dob=$row["dob"];
-    $course=$row["course"];
-    $region=$row["region"];
-    $yearOfAdmission=$row["yearOfAdmission"];
-    $yearOfCompletion=$row["yearOfCompletion"];
-    $rank1=$row["rank"];
-    $program=$row["course"];
+    $staffID = $row["staffID"];
+    $fName = $row["fName"];
+    $mName = $row["mName"];
+    $lName = $row["lName"];
+    $mobile = $row["mobile"];
+    $fullName = $fName . " " . $mName . " " . $lName;
 
 
 
-   // echo $staffID;
-   // exit;
+    $gender = $row["gender"];
+    $ghanaCard = $row["ghanaCard"];
+    $email = $row["email"];
+    $ssnit = $row["ssnitNumber"];
+    $studyLeaveStatus = $row["studyLeaveStatus"];
+    $admissionNumber = $row["admissionNumber"];
+    $regNumber = $row["regNumber"];
+    $level = $row["level"];
+    $dob = $row["dob"];
+    $course = $row["course"];
+    $region = $row["region"];
+    $yearOfAdmission = $row["yearOfAdmission"];
+    $yearOfCompletion = $row["yearOfCompletion"];
+    $rank1 = $row["rank"];
+    $program = $row["course"];
+
+
+
+    // echo $staffID;
+    // exit;
 
 
     // CREATE Excel
 
 
-    if(!empty($staffID)){
-      $activeSheet->setCellValue('A' .$row2, $row2-1);
-      $activeSheet->setCellValue('B' .$row2, $staffID);
-      $activeSheet->setCellValue('C' .$row2, $fName);
-      $activeSheet->setCellValue('D' .$row2, $mName);
-      $activeSheet->setCellValue('E' .$row2, $lName);
-      $activeSheet->setCellValue('F' .$row2, $gender);
-      $activeSheet->setCellValue('G' .$row2, $mobile);
-      $activeSheet->setCellValue('H' .$row2, $ghanaCard);
-      $activeSheet->setCellValue('I' .$row2, $email);
-      $activeSheet->setCellValue('J' .$row2, $ssnit);
-      $activeSheet->setCellValue('K' .$row2, $studyLeaveStatus);
-      $activeSheet->setCellValue('L' .$row2, $admissionNumber);
-      $activeSheet->setCellValue('M' .$row2, $regNumber);
-      $activeSheet->setCellValue('N' .$row2, $level);
-      $activeSheet->setCellValue('O' .$row2, $dob);
-      $activeSheet->setCellValue('P' .$row2, $course);
-      $activeSheet->setCellValue('Q' .$row2, $region);
-      $activeSheet->setCellValue('R' .$row2, $yearOfAdmission);
-      $activeSheet->setCellValue('S' .$row2, $yearOfCompletion);
-      $activeSheet->setCellValue('T' .$row2, $rank1);
+    if (!empty($staffID)) {
+      $activeSheet->setCellValue('A' . $row2, $row2 - 1);
+      $activeSheet->setCellValue('B' . $row2, $staffID);
+      $activeSheet->setCellValue('C' . $row2, $fName);
+      $activeSheet->setCellValue('D' . $row2, $mName);
+      $activeSheet->setCellValue('E' . $row2, $lName);
+      $activeSheet->setCellValue('F' . $row2, $gender);
+      $activeSheet->setCellValue('G' . $row2, $mobile);
+      $activeSheet->setCellValue('H' . $row2, $ghanaCard);
+      $activeSheet->setCellValue('I' . $row2, $email);
+      $activeSheet->setCellValue('J' . $row2, $ssnit);
+      $activeSheet->setCellValue('K' . $row2, $studyLeaveStatus);
+      $activeSheet->setCellValue('L' . $row2, $admissionNumber);
+      $activeSheet->setCellValue('M' . $row2, $regNumber);
+      $activeSheet->setCellValue('N' . $row2, $level);
+      $activeSheet->setCellValue('O' . $row2, $dob);
+      $activeSheet->setCellValue('P' . $row2, $course);
+      $activeSheet->setCellValue('Q' . $row2, $region);
+      $activeSheet->setCellValue('R' . $row2, $yearOfAdmission);
+      $activeSheet->setCellValue('S' . $row2, $yearOfCompletion);
+      $activeSheet->setCellValue('T' . $row2, $rank1);
 
     }
 
     $row2++;
 
 
-    $return_arr[]= array(
+    $return_arr[] = array(
       "staffID" => $staffID,
       "fullName" => $fullName,
       "mobile" => $mobile
-  );
+    );
 
   }
 }
@@ -214,37 +214,18 @@ if ($result->num_rows > 0) {
 
 
 $json = json_encode($return_arr);
-if(json_last_error() === JSON_ERROR_NONE){
-    echo $json;
-}else{
-   // echo json_encode(array("error"=>"An error occurred while encoding data to json"));
+if (json_last_error() === JSON_ERROR_NONE) {
+  echo $json;
+} else {
+  // echo json_encode(array("error"=>"An error occurred while encoding data to json"));
 }
 
 
 
 
-  $writer = new PhpOffice\PhpSpreadsheet\Writer\Xlsx($spreadsheet);
+$writer = new PhpOffice\PhpSpreadsheet\Writer\Xlsx($spreadsheet);
 
 
-  $writer->save('file/Records.xlsx');
+$writer->save('file/Records.xlsx');
 
-  $conn->close();
-
-
-
-  
-  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+$conn->close();
